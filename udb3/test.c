645f8fb9718d51e1a027b6c5971db3cc86ba7c02
Patrick Pelissier <Patrick.Pelissier@gmail.com>
2024-05-08 17:16:13 +0200

    Update

--- udb3/test.c
+++ udb3/test.c
@@ -122,6 +122,89 @@ void bulk_add(intmap_t dict, unsigned n, uint32_t val[n], uint32_t key[n], uint3
   }
 }
 
+volatile unsigned g_counter;
+static inline void update1(intmap_t h, uint32_t key, uint32_t *oldval, uint32_t *newval)
+{
+  (void) key;
+  *newval += *oldval;
+  *oldval = *newval;
+}
+
+void bulk_update(intmap_t dict, unsigned n, uint32_t val[n], uint32_t key[n], uint32_t def,
+                 void (*update)(intmap_t dict, uint32_t key, uint32_t *newval, uint32_t *preval))
+{
+  size_t hash[MAX_PREFETCH];
+  M_ASSUME(n >= MAX_PREFETCH);
+  for(unsigned i = 0 ; i < MAX_PREFETCH; i++) {
+    hash[i] = udb_hash_fn(key[i]);
+    __builtin_prefetch(&dict->data[hash[i] & dict->mask], 0, 0);
+  }
+  
+  for (unsigned i = 0; i < n; i++) {
+    unsigned long k = key[i];
+    size_t p = hash[i % MAX_PREFETCH] & dict->mask;
+    if (i < n-MAX_PREFETCH) {
+      hash[i % MAX_PREFETCH] = udb_hash_fn(key[i+MAX_PREFETCH]);
+      __builtin_prefetch(&dict->data[hash[i % MAX_PREFETCH] & dict->mask], 0, 0);
+    }
+    unsigned long ref = dict->data[p].key;
+    if (M_LIKELY (ref == k)) {
+      update(dict, k, &dict->data[p].value, &val[i]);
+      goto next;     
+    }
+    // Search loop for insertion
+    size_t s = 1;
+    size_t delPos = SIZE_MAX;
+    while (M_UNLIKELY (!oor_equal_p (dict->data[p].key, M_D1CT_OA_EMPTY)) ) {
+      if (dict->data[p].key == k) {
+        // Found, no need for insertion
+        update(dict, k, &dict->data[p].value, &val[i]);
+        goto next;
+      }
+      if (oor_equal_p (dict->data[p].key, M_D1CT_OA_DELETED)
+          && (delPos == SIZE_MAX))                          
+        delPos = p;                                                     
+      p = (p + M_D1CT_OA_PROBING(s)) & dict->mask;
+    }
+    // Add new items
+    if (delPos != SIZE_MAX) {                                           
+      p = delPos;                                                       
+      dict->count_delete --;                                            
+    }
+    dict->data[p].key = k;
+    dict->data[p].value = def;
+    update(dict, k, &dict->data[p].value, &val[i]);
+    dict->count++;                                                      
+    dict->count_delete ++;                                                    
+                                                                              
+    if (M_UNLIKELY (dict->count_delete >= dict->upper_limit)) {               
+      size_t newSize = dict->mask+1;                                          
+      if (dict->count > (dict->mask / 2)) {                                   
+        newSize += newSize;                                                   
+        if (M_UNLIKELY_NOMEM (newSize <= dict->mask+1)) {                     
+          M_MEMORY_FULL((size_t)-1);                                          
+        }                                                               
+      }                                                                 
+      m_d1ct_intmap_resize_up(dict, newSize, true);
+      continue;
+    } 
+  next:
+    // FIXME: Needed? To measure.
+    clflush(& dict->data[p].key);
+  }
+}
+
+static inline void update2(intmap_t h, uint32_t key, uint32_t *oldval, uint32_t *newval)
+{
+  if (*oldval == 0xFFFFFFFF) {
+    *oldval = *newval;
+    *newval = 1;
+  } else {
+    intmap_erase(h, key);
+    *newval = 0;
+  }
+}
+
 void test_int(uint32_t N, uint32_t n0, int32_t is_del, uint32_t x0, uint32_t n_cp, udb_checkpoint_t *cp)
 {
   uint32_t step = (N - n0) / (n_cp - 1);
@@ -132,21 +215,44 @@ void test_int(uint32_t N, uint32_t n0, int32_t is_del, uint32_t x0, uint32_t n_c
   uint32_t values[BUF_N];
   uint32_t keys[BUF_N];
   intmap_init(h);
-  if (is_del) abort();
   for (j = 0, i = 0, n = n0; j < n_cp; ++j, n += step) {
-    while (i < n) {
-      unsigned num = M_MIN(BUF_N, n-i);
-      for(unsigned k = 0; k < num ; k++) {
-        values[k] = 1;
-        uint64_t y = udb_splitmix64(&x);
-        uint32_t key = udb_get_key(n, y);
-        keys[k] = key;
-      }          
-      bulk_add(h, num, values, keys, 0);
-      for(unsigned k = 0; k < num ; k++) {
-        z += values[k];
+    if (is_del) {
+#if BULK_MODE == 1
+      abort();
+#endif
+      while (i < n) {
+        unsigned num = M_MIN(BUF_N, n-i);
+        for(unsigned k = 0; k < num ; k++) {
+          values[k] = i + k;
+          uint64_t y = udb_splitmix64(&x);
+          uint32_t key = udb_get_key(n, y);
+          keys[k] = key;
+        }
+        bulk_update(h, num, values, keys, -1, update2);
+        for(unsigned k = 0; k < num ; k++) {
+          z += values[k];
+        }
+        i += num;
       }
-      i += num;
+    } else {
+      while (i < n) {
+        unsigned num = M_MIN(BUF_N, n-i);
+        for(unsigned k = 0; k < num ; k++) {
+          values[k] = 1;
+          uint64_t y = udb_splitmix64(&x);
+          uint32_t key = udb_get_key(n, y);
+          keys[k] = key;
+        }
+#if BULK_MODE == 1
+        bulk_add(h, num, values, keys, 0);
+#elif BULK_MODE == 2
+        bulk_update(h, num, values, keys, 0, update1);
+#endif
+        for(unsigned k = 0; k < num ; k++) {
+          z += values[k];
+        }
+        i += num;
+      }
     }
     udb_measure(n, intmap_size(h), z, &cp[j]);
   }
